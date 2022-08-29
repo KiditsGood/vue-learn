7e0bf58b903ded3bb00a9a8e95881d59c9ebf51b
Oleg Sadchikov <71182254+KiditsGood@users.noreply.github.com>
2022-08-29 11:35:39 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app">
+    <PostForm />
+    <PostList :posts="posts" />
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+
+export default {
+  components: {
+    PostList,
+    PostForm
+  },
+
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Описание поста'},
+        {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+        {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'}
+      ],
+      title: '',
+      body: ''
+    }
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost)
+      this.title = ''
+      this.body = ''
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input v-model="post.title" type="text" placeholder="Название поста">
+    <input v-model="post.body" type="text" placeholder="Описание поста">
+    <button @click="createPost">Создание поста</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
